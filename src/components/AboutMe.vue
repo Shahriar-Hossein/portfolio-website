2db4ee21fe5dbc889656ea276d2d1a2732f2e130
Shahriar-Hossein <shahriar2507@gmail.com>
2025-03-23 07:02:16 +0600

    created about me section

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-between p-2 m-2">
+    <div class="grow mx-auto px-16">
+      <p class="text-4xl text-white font-bold">Md Shahriar Hossein</p>
+      <p class="text-xl text-gray-100 font semibold">Software Engineer</p>
+      <p>I build softwares</p>
+    </div>
+
+    <div class="mx-auto">
+      <p class="py-2">
+        I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+        blend thoughtful design with robust engineering. My favorite work lies at the intersection
+        of design and development, creating experiences that not only look great but are
+        meticulously built for performance and usability.
+      </p>
+      <p class="py-2">
+        I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+        blend thoughtful design with robust engineering. My favorite work lies at the intersection
+        of design and development, creating experiences that not only look great but are
+        meticulously built for performance and usability.
+      </p>
+      <p class="py-2">
+        I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+        blend thoughtful design with robust engineering. My favorite work lies at the intersection
+        of design and development, creating experiences that not only look great but are
+        meticulously built for performance and usability.
+      </p>
+      <p class="py-2">
+        I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+        blend thoughtful design with robust engineering. My favorite work lies at the intersection
+        of design and development, creating experiences that not only look great but are
+        meticulously built for performance and usability.
+      </p>
+    </div>
+  </div>
+</template>
