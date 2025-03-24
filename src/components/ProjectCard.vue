64dd87464b94fdca8703cf914a1a2e2461aa2436
Shahriar-Hossein <shahriar2507@gmail.com>
2025-03-25 03:06:56 +0600

    changed title color to cyan

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,7 +13,7 @@ defineProps<{ project: Project }>();
 <div class="max-w-sm m-2 lg:m-4 hover:-translate-2 border rounded-lg shadow-sm shadow-cyan-500/50 bg-gray-800 border-gray-700 hover:shadow-xl">
   <img class="rounded-t-lg" :src="project.image" alt="" />
   <div class="p-5">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ project.title }}</h5>
+    <h5 class="mb-2 text-2xl text-cyan-400 font-bold tracking-tight">{{ project.title }}</h5>
     <p class="mb-3 font-normal text-gray-400">{{ project.description }}</p>
     <a :href="project.link" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-600 rounded-lg hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-cyan-800">
       Github Repository
