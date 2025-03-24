44789ae86f7842775016bd29f139a1123c60c519
Shahriar-Hossein <shahriar2507@gmail.com>
2025-03-25 03:05:29 +0600

    using cyan as the theme color

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -3,7 +3,7 @@
 <template>
   <section class="container mx-auto flex-col items-center place-content-center py-26">
     <div class="mx-32">
-      <h1 class="text-4xl text-white font-bold my-12"> About me </h1>
+      <h1 class="text-4xl text-cyan-400 font-bold my-12"> About me </h1>
       <div class="flex justify-between content-start">
         <div class="pr-12">
           <p class="mt-2">
@@ -24,7 +24,7 @@
         </div>
         <div class="px-22">
           <img
-            class="h-auto max-w-3xs mx-auto transition-all duration-300 rounded-xl opacity-30 hover:opacity-90 shadow-xl shadow-gray-800"
+            class="h-auto max-w-3xs mx-auto transition-all duration-300 rounded-xl opacity-30 hover:opacity-100 shadow-xl shadow-cyan-500/50"
             src="../assets/IMG-20250217-WA0004.jpg"
             alt="My Picture"
           />
