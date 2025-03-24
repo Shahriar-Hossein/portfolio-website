1a51a87a1baf37d1a723aaf9d5790e95f8dc89b1
Shahriar-Hossein <shahriar2507@gmail.com>
2025-03-24 07:18:58 +0600

    designed hero section and about section

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="container mx-auto flex-col items-center place-content-center py-24">
+    <div class="mx-32">
+      <h1 class="text-4xl text-white font-bold my-12"> About me </h1>
+      <div class="flex justify-between content-start">
+        <div class="pr-12">
+          <p class="mt-2">
+            Hello! My name is Shahriar. I am software engineer
+          </p>
+          <p class="mt-2">
+            I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+            blend thoughtful design with robust engineering.
+          </p>
+          <p class="mt-2">
+            I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+            blend thoughtful design with robust engineering.
+          </p>
+          <p class="mt-2">
+            I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that
+            blend thoughtful design with robust engineering.
+          </p>
+        </div>
+        <div class="px-22">
+          <img
+            class="h-auto max-w-3xs mx-auto transition-all duration-300 rounded-xl opacity-30 hover:opacity-90 shadow-xl shadow-gray-800"
+            src="../assets/IMG-20250217-WA0004.jpg"
+            alt="My Picture"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="container flex-col items-center place-content-center text-lg min-h-screen">
+    <div class="mx-32">
+      <h1 class="text-7xl font-bold">Md Shahriar Hossein</h1>
+      <h4 class="mt-2 text-gray-400 text-4xl font semibold">Software Engineer</h4>
+      <p class="pt-2">I build softwares</p>
+    </div>
+  </section>
+</template>
