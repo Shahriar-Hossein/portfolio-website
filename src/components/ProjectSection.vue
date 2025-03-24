e5454b80f641a382f81b8a00f5b27bc992bac942
Shahriar-Hossein <shahriar2507@gmail.com>
2025-03-25 01:00:20 +0600

    basic design of project cards

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProjectCard from './ProjectCard.vue';
+
+</script>
 
 <template>
   <section class="container mx-auto flex-col items-center place-content-center py-26">
     <div class="mx-32">
       <h1 class="text-4xl text-white font-bold my-12"> Projects... </h1>
-      <div>
-
+      <div class="flex flex-wrap items-center justify-center">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </div>
     </div>
   </section>
