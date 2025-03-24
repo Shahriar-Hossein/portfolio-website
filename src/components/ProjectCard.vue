18cfc6054ecaad8d6c3a25a36bb7d64b6775580e
Shahriar-Hossein <shahriar2507@gmail.com>
2025-03-25 03:05:11 +0600

    made project section dynamic, using props. added cyan color

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,16 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Project {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}
+defineProps<{ project: Project }>();
+
+</script>
 
 <template>
-<div class="max-w-sm bg-white m-4 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-  <!-- <a href="#"> -->
-    <img class="rounded-t-lg" src="../assets/nature.jpg" alt="" />
-  <!-- </a> -->
+<div class="max-w-sm m-2 lg:m-4 hover:-translate-2 border rounded-lg shadow-sm shadow-cyan-500/50 bg-gray-800 border-gray-700 hover:shadow-xl">
+  <img class="rounded-t-lg" :src="project.image" alt="" />
   <div class="p-5">
-    <!-- <a href="#"> -->
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    <!-- </a> -->
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ project.title }}</h5>
+    <p class="mb-3 font-normal text-gray-400">{{ project.description }}</p>
+    <a :href="project.link" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-600 rounded-lg hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-cyan-800">
       Github Repository
       <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,23 +1,58 @@
 <script setup lang="ts">
 import ProjectCard from './ProjectCard.vue';
 
+interface Project {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}
+
+const projects: Project[] = [
+  {
+    title: "E-Learning Platform",
+    description: "This is website for online courses. Teachers can create courses, quizes, video lessons. Students can access course materials after buying the course through online payment.",
+    link: "github.come",
+    image: "/nature.jpg"
+  },
+  {
+    title: "Rental Book Service",
+    description: "This is website for online courses. Teachers can create courses, quizes, video lessons. Students can access course materials after buying the course through online payment.",
+    link: "github.come",
+    image: "/nature.jpg"
+  },
+  {
+    title: "Car Shop",
+    description: "This is website for online courses. Teachers can create courses, quizes, video lessons. Students can access course materials after buying the course through online payment.",
+    link: "github.come",
+    image: "/nature.jpg"
+  },
+  {
+    title: "University automation",
+    description: "This is website for online courses. Teachers can create courses, quizes, video lessons. Students can access course materials after buying the course through online payment.",
+    link: "github.come",
+    image: "/nature.jpg"
+  },
+  {
+    title: "Project Loading",
+    description: "This is website for online courses. Teachers can create courses, quizes, video lessons. Students can access course materials after buying the course through online payment.",
+    link: "github.come",
+    image: "/nature.jpg"
+  },
+];
+
 </script>
 
 <template>
   <section class="container mx-auto flex-col items-center place-content-center py-26">
     <div class="mx-32">
-      <h1 class="text-4xl text-white font-bold my-12"> Projects... </h1>
+      <h1 class="text-4xl text-cyan-400 font-bold my-12"> Projects </h1>
       <div class="flex flex-wrap items-center justify-center">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+          <ProjectCard
+            v-for="project in projects"
+            :key="project.title"
+            :project="project"
+          />
       </div>
     </div>
   </section>
