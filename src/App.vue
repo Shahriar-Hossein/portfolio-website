207452cf2568a6559a26df5c1bc4a785a1d7c2f1
Shahriar-Hossein <shahriar2507@gmail.com>
2025-04-02 13:23:10 +0600

    added basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import AboutSection from './components/AboutSection.vue';
 import HeroSection from './components/HeroSection.vue';
+import NavBar from './components/NavBar.vue';
 import ProjectSection from './components/ProjectSection.vue';
 </script>
 
 <template>
   <div class="bg-gray-900 text-gray-100 text-lg">
-    <!-- <header>
-
-    </header> -->
+    <header>
+      <NavBar />
+    </header>
 
     <main class="flex-col justify-items-center px-40">
       <HeroSection />
