298525ca8e383c2d1915aa2e6ebf701762d804d9
Shahriar-Hossein <shahriar2507@gmail.com>
2025-06-06 00:47:40 +0600

    improved responsive design

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -8,63 +8,66 @@ defineExpose({
   experienceRef,
 })
 
-
 const selectedExp = ref(0);
 const showExp = ref(experiences[selectedExp.value]);
 const handleSelection = (index: number) =>{
   selectedExp.value = index;
   showExp.value = experiences[selectedExp.value];
-  console.log(showExp.value);
-
 }
-
 </script>
 
 <template>
   <section
     ref="experienceRef"
     id="experience"
-    class="container mx-auto flex-col items-center place-content-center py-26"
+    class="container mx-auto py-20 px-4 md:px-8"
   >
-    <div class="lg:mx-32">
-      <h1 class="text-4xl text-cyan-400 font-bold my-12">Experience</h1>
-      <div class="flex gap-12 items-start">
-        <div class="h-92 overflow-y-auto">
-          <ul class="">
-            <!-- future design -->
-            <!-- <li
-              class="bg-cyan-500 cursor-pointer rounded-tl-full rounded-br-full hover:rounded-3xl transition-rounded duration-500 ease-out text-white text-sm text-center self-center px-4 py-2"
-            >
-              DevSpace
-            </li> -->
+    <div class="mx-auto max-w-5xl">
+      <h1 class="text-3xl md:text-4xl text-cyan-400 font-bold mb-10 text-center md:text-left">Experience</h1>
 
-            <li
-              v-for="(experience, index) in experiences"
-              :key="experience.company_name"
-              @click="handleSelection(index)"
-              :class="['cursor-pointer text-sm text-center self-center px-4 py-2 m-2 rounded-xl transition-all duration-300',
-                selectedExp == index
-                ? 'bg-cyan-600 text-white ring-2 ring-cyan-400/60'
-                : 'text-cyan-300 hover:text-white hover:bg-cyan-800'
-              ]">
-              {{ experience.company_name }}
-            </li>
-          </ul>
-        </div>
-        <div class="flex-1 mx-2 h-92 overflow-y-auto">
-          <div class="pb-2 text-xl">
+      <div class="flex flex-col md:flex-row md:gap-8">
+        <!-- Company names -->
+        <ul class="flex md:flex-col overflow-x-auto md:h-64 md:overflow-visible">
+          <!-- future design -->
+          <!-- <li
+            class="bg-cyan-500 cursor-pointer rounded-tl-full rounded-br-full hover:rounded-3xl transition-rounded duration-500 ease-out text-white text-sm text-center self-center px-4 py-2"
+          >
+            DevSpace
+          </li> -->
+
+          <li
+            v-for="(experience, index) in experiences"
+            :key="experience.company_name"
+            @click="handleSelection(index)"
+            :class="['cursor-pointer whitespace-nowrap text-sm md:text-left px-4 py-2 rounded-lg transition-all duration-300',
+              selectedExp == index
+              ? 'bg-cyan-600 text-white ring-2 ring-cyan-400/60'
+              : 'text-cyan-300 hover:text-white hover:bg-cyan-800'
+            ]">
+            {{ experience.company_name }}
+          </li>
+        </ul>
+        <!-- Description -->
+        <div class="md:flex-1 mx-2 ">
+          <div class="text-xl font-bold">
             <span>{{ showExp?.designation }}</span>
             <span> - </span>
-            <span>{{ showExp?.company_name }}</span>
+            <a
+              :href="showExp?.company_link ?? '#'"
+              target="_blank"
+              class="text-cyan-500 hover:underline transition-all duration-300 ease-in"
+            >
+              <span>{{ showExp?.company_name }}</span>
+            </a>
           </div>
-          <div class="pb-2 text-sm">
+          <div class="pb-2 text-sm italic text-gray-400">
             <span>{{ showExp?.time }}</span>
           </div>
-          <ul class="list-none">
+          <ul class="list-none min-h-92">
             <li
               v-for="bullet in showExp?.bullets"
               :key="bullet"
-              class="relative pl-5 before:absolute before:left-0 before:top-3 before:h-2 before:w-2 before:rounded-full before:bg-cyan-500"
+              class="relative pl-5 before:absolute before:left-0 before:top-3 before:h-2 before:w-2 before:rounded-full before:bg-cyan-500 mb-1"
             >
               {{ bullet }}
             </li>
