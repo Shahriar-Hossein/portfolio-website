b4a3ebf06bee6ff99ddd4844b7c0bbd7c73b19f8
Shahriar-Hossein <shahriar2507@gmail.com>
2025-06-06 02:07:37 +0600

    added show more button for exploring more projects

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useTemplateRef } from 'vue';
+import { ref, useTemplateRef } from 'vue';
 import ProjectCard from './ProjectCard.vue';
 import { projects } from '@/data/projects';
 
@@ -8,6 +8,11 @@ const projectsRef = useTemplateRef<HTMLElement | null>('projectsRef');
 defineExpose({
   projectsRef
 });
+
+const limit = ref(3);
+const handleShowMore = ()=>{
+  limit.value += 6;
+}
 
 </script>
 
@@ -19,12 +24,27 @@ defineExpose({
   >
     <div class="mx-auto max-w-5xl">
       <h1 class="text-3xl md:text-4xl text-cyan-400 font-bold mb-10 text-center md:text-left"> Projects </h1>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-start">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center">
           <ProjectCard
-            v-for="project in projects"
+            v-for="project in projects.slice(0,limit)"
             :key="project.title"
             :project="project"
           />
+      </div>
+      <div class="flex justify-center mt-6">
+        <button
+          v-if="limit<projects.length"
+          @click="handleShowMore"
+          class="inline-flex cursor-pointer items-center justify-center text-sm text-white bg-cyan-600 hover:bg-cyan-700 rounded-md px-4 py-2 transition-all duration-200"
+        >
+          Show More
+        </button>
+        <button
+          v-else
+          class="inline-flex cursor-not-allowed items-center justify-center text-sm text-white bg-cyan-950 rounded-md px-4 py-2 transition-all duration-200"
+        >
+          No more projects
+        </button>
       </div>
     </div>
   </section>
