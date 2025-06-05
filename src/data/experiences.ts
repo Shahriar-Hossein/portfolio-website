77115ad3d0b92e5b9b8033e0d3c21adb2fca0267
Shahriar-Hossein <shahriar2507@gmail.com>
2025-06-05 21:44:10 +0600

    added experience data

---
+++ src/data/experiences.ts
@@ -0,0 +1,72 @@
+import type { Experience } from '@/interfaces/Experience'
+
+export const experiences: Experience[] = [
+  {
+    designation: 'Junior Software Engineer',
+    company_name: 'DevSpace',
+    company_link: 'https://www.linkedin.com/company/devspacebd/',
+    time: 'September 2024 - August 2024',
+    bullets: [
+      'Delivered 90+ API endpoints following REST conventions across 3 projects within 7 months in Laravel.',
+      'Automated API documentation using L5-Swagger, reducing integration time by 25%.',
+      'Integrated OAuth login via Google and Facebook using Laravel Socialite for streamlined user authentication.',
+      'Developed office management system using Laravel+Vuejs with automated attendance tracking and leave management, for the devspace team saving 5+ weekly hours.',
+      'Co-developed a Next.js + React company portfolio website, enhancing brand visibility and online presence.',
+      'Implemented a theme-switching system with multiple color schemes to improve user experience.',
+      'Developed a blog application with profiles, posts, nested comments and image uploads using Ruby on Rails.'
+
+    ],
+  },
+  {
+    designation: 'Programming Trainer',
+    company_name: 'Programming Club, IUBAT',
+    company_link: 'https://www.facebook.com/groups/1689495774649321',
+    time: 'May 2022 - December 2023',
+    bullets: [
+      'Taught C/C++ fundamentals and problem-solving skills to 100+ beginner students over multiple semesters.',
+      'Designed and led a two-tier training program, transitioning students from basic syntax to competitive programming in C++.',
+      'Designed and led a two-tier training program, transitioning students from basic syntax to competitive programming in C++.',
+      'Contributed to the planning and execution of 5+ live programming competitions as a volunteer',
+      'Developed structured lesson plans and session schedule to ensure consistent, progressive learning outcomes.'
+    ],
+  },
+  {
+    designation: 'Software Engineer (Part-Time)',
+    company_name: 'Sharksss Capital Inc',
+    company_link: 'https://www.facebook.com/profile.php?id=61576568017984',
+    time: 'June 2025 - Present',
+    bullets: [
+      'Leading the development of the company’s full-featured website for a forex trading platform, with role-based access for students, investors, and administrators.',
+      'Planning and building core modules, including course purchasing and access for students, investment handling for investors, and a management dashboard for admins.',
+      'Collaborating closely with stakeholders to define features such as team profiles, trading blogs, course listings, and a multimedia gallery.',
+      'Interviewed and selected web development interns; will lead and mentor them throughout the development cycle.',
+      'Responsible for maintaining clean, scalable architecture while ensuring security, usability, and performance of the platform.'
+
+    ],
+  },
+  {
+    designation: 'Founder & CEO',
+    company_name: 'Rental Books Service',
+    company_link: 'https://www.facebook.com/profile.php?id=100091935829821',
+    time: 'May 2023 - Present',
+    bullets: [
+      'Founded and scaled a semester-based book rental service for university students.',
+      'Managed procurement, pricing, logistics, and customer operations independently.',
+      'Handled orders, inventory, and returns, with payments processed via bKash.',
+      'Grew the service using a Facebook page as the central communication and sales platform.',
+      'Led a three-person team handling delivery, logistics, and branding.'
+    ],
+  },
+  {
+    designation: 'Executive Member',
+    company_name: 'BASIS-IUBAT',
+    company_link: 'https://www.facebook.com/basisstudentsforumiubat',
+    time: 'May 2022 - October 2024',
+    bullets: [
+      'Collaborated on planning and executing technical workshops and seminars, including hands-on sessions on Git & GitHub and webinars on internships and career development.',
+      'Promoted events through coordinated on-campus campaigns and digital outreach, boosting engagement and attendance among students.',
+      'SContributed to event setup and branding — decorating venues with banners and balloons, arranging logistics, and ensuring a welcoming, professional atmosphere.',
+      'Volunteered in event-day operations, including attendee registration, speaker support, and smooth flow of sessions.'
+    ],
+  },
+]
