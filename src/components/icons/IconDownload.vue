de9e861803fcf211cdb6e334dc10b2e4f041b81d
Shahriar-Hossein <shahriar2507@gmail.com>
2025-06-06 17:17:57 +0600

    added download icon

---
+++ src/components/icons/IconDownload.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 24 24" id="download">
+  <g id="Duotone">
+    <path fill="#FFF" d="m21.5 18-10 5.5-10-5.5V7l10-5.5 10 5.5z" style="fill:#fff"></path>
+    <path fill="#CCE7FF" d="m19.5 16.5-8 4.5-8-4.5-2 1.5 10 5.5 10-5.5z" style="fill:#cce7ff"></path>
+    <path d="m15.5 13.5-4 4-4-4M11.5 17.5v-10" style="fill:none;stroke:#1078ff;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10"></path>
+    <path d="m21.5 18-10 5.5-10-5.5V7l10-5.5 10 5.5z" style="fill:none;stroke:#1078ff;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10"></path>
+  </g>
+  <path id="Frames-24px" fill="none" d="M0 0h24v24H0z" style="fill:none"></path>
+</svg>
+</template>
